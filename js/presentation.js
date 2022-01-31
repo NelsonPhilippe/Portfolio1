09d720da3894740bf4109bcd9e26d17f059f7d49
XiliTra <philippenelson59@gmail.com>
2022-01-31 15:36:54 +0100

    add experience enterprise

---
+++ js/presentation.js
@@ -0,0 +1,8 @@
+const about = document.getElementById('about')
+const container = document.getElementById('container-p')
+
+
+$(window).on('load', () => {
+    about.style.top = "10vh"
+    container.style.marginLeft = "10vh"
+})
